260bd89ff343df115995d771320225b0de86dd8b
Jesse Cusack <jesse.cusack@noc.soton.ac.uk>
2020-01-31 08:11:43 -0800

    more interface analysis

--- analysis/interface_analysis.py
+++ analysis/interface_analysis.py
@@ -30,6 +30,8 @@ data_dir = "../no_tide/run1"
 grid = xr.open_dataset(os.path.join(data_dir, grid_file))
 data_dir = "../interface_height_down/run"
 dat_down = xr.open_dataset(os.path.join(data_dir, diag1_file))
+data_dir = "../interface_height_down50/run"
+dat_down50 = xr.open_dataset(os.path.join(data_dir, diag1_file))
 data_dir = "../interface_height_up/run"
 dat_up = xr.open_dataset(os.path.join(data_dir, diag1_file))
 data_dir = "../no_tide/run1"
@@ -50,7 +52,7 @@ ax.axvline(101)
 iy = 850
 zslice = slice(70, 130)
 tslice = slice(0, len(dat_down.T))
-fig, axs = plt.subplots(3, 1, sharex=True, sharey=True)
+fig, axs = plt.subplots(4, 1, sharex=True, sharey=True)
 axs[0].set_ylim(grid.Z[105], grid.Z[70])
 axs[0].contourf(
     dat_down.T[tslice],
@@ -59,20 +61,27 @@ axs[0].contourf(
     np.arange(-10, -4),
 )
 axs[0].annotate("down 100 m", (0.1, 0.8), xycoords="axes fraction")
-C = axs[1].contourf(
+axs[1].contourf(
+    dat_down50.T[tslice],
+    grid.Z[zslice],
+    np.log10(dat_down50.KLeps[tslice, zslice, iy, 0].T),
+    np.arange(-10, -4),
+)
+axs[1].annotate("down 50 m", (0.1, 0.8), xycoords="axes fraction")
+C = axs[2].contourf(
     dat_nc.T[tslice],
     grid.Z[zslice],
     np.log10(dat_nc.KLeps[tslice, zslice, iy, 0].T),
     np.arange(-10, -4),
 )
-axs[1].annotate("no change", (0.1, 0.8), xycoords="axes fraction")
-axs[2].contourf(
+axs[2].annotate("no change", (0.1, 0.8), xycoords="axes fraction")
+axs[3].contourf(
     dat_up.T[tslice],
     grid.Z[zslice],
     np.log10(dat_up.KLeps[tslice, zslice, iy, 0].T),
     np.arange(-10, -4),
 )
-axs[2].annotate("up 100 m", (0.1, 0.8), xycoords="axes fraction")
+axs[3].annotate("up 100 m", (0.1, 0.8), xycoords="axes fraction")
 plt.colorbar(C, cax=fig.add_axes((0.95, 0.2, 0.01, 0.6)), orientation="vertical")
 
 # %%
@@ -84,6 +93,7 @@ ax.plot(
     dat_nc.T,
     itr.cumtrapz(dat_nc.KLeps[:, zslice, iy, 0].mean(axis=1), dat_nc.T, initial=0)
     / dat_nc.T,
+    label='no change',
 )
 ax.plot(
     dat_down.T[tslice],
@@ -93,6 +103,27 @@ ax.plot(
         initial=0,
     )
     / dat_down.T[tslice],
+    label='down 100 m',
+)
+ax.plot(
+    dat_down.T[tslice],
+    itr.cumtrapz(
+        dat_down50.KLeps[tslice, zslice, iy, 0].mean(axis=1),
+        dat_down50.T[tslice],
+        initial=0,
+    )
+    / dat_down.T[tslice],
+    label='down 50 m',
+)
+ax.plot(
+    dat_up.T[tslice],
+    itr.cumtrapz(
+        dat_up.KLeps[tslice, zslice, iy, 0].mean(axis=1),
+        dat_up.T[tslice],
+        initial=0,
+    )
+    / dat_up.T[tslice],
+    label='up 100 m',
 )
 ax.plot(
     dat_nc.T[tslice],
@@ -100,7 +131,9 @@ ax.plot(
         dat_nc.KLeps[tslice, zslice, iy, 0].mean(axis=1), dat_nc.T[tslice], initial=0
     )
     / dat_nc.T[tslice],
+    ':k',
 )
+ax.legend()
 
 # %%
 eps_down = dat_down.KLeps.mean(axis=(1, 2, 3))
@@ -113,10 +146,10 @@ fig, axs = plt.subplots(2, 1, figsize=(6.5, 4), sharex=True)
 # axs[0].plot(diag1['T'], 2*np.sin(2*np.pi*diag1['T']/44640))
 axs[0].plot(dat_down["T"], eps_down, label="100 m lower interface")
 axs[0].plot(dat_nc["T"], eps_nc, label="normal interface")
-axs[0].plot(dat_up["T"], eps_up, label="100 m higher interface")
+# axs[0].plot(dat_up["T"], eps_up, label="100 m higher interface")
 axs[1].plot(dat_down["T"], itr.cumtrapz(eps_down, dat_down["T"], initial=0))
 axs[1].plot(dat_nc["T"], itr.cumtrapz(eps_nc, dat_nc["T"], initial=0))
-axs[1].plot(dat_up["T"], itr.cumtrapz(eps_up, dat_up["T"], initial=0))
+# axs[1].plot(dat_up["T"], itr.cumtrapz(eps_up, dat_up["T"], initial=0))
 axs[0].legend()
 
 axs[0].set_ylabel("Mean $\epsilon$\n(W kg$^{-1}$)")
@@ -127,3 +160,20 @@ name = "comparison.png"
 #             dpi=200)
 
 # %%
+dats = [dat_down, dat_down50, dat_up, dat_nc]
+names = ['down 100 m', 'down 50 m', 'up 100 m', 'no change']
+
+izs = 60
+
+fig, ax = plt.subplots(1, 1)
+for i, dat in enumerate(dats):
+    dTdz = np.gradient(dat.THETA[0, izs:, 0, 0], grid.Z[izs:])
+    ax.plot(dTdz, grid.Z[izs:], label=names[i])
+    
+ax.legend()
+
+fig, ax = plt.subplots(1, 1)
+for i, dat in enumerate(dats):
+    ax.plot(dat.THETA[0, izs:, 0, 0], grid.Z[izs:], label=names[i])
+    ax.plot(dat.THETA[0, izs:, -1, 0], grid.Z[izs:], label=names[i])
+ax.legend()
