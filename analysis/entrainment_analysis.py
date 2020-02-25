2b90ed261efeae1322c3fbe708ee9eda5bf13572
Jesse Cusack <jesse.cusack@noc.soton.ac.uk>
2020-02-25 15:32:03 -0800

    figures and analysis for OSM

--- analysis/entrainment_analysis.py
+++ analysis/entrainment_analysis.py
@@ -35,10 +35,12 @@ data_dir = "../interface_height_down50/run"
 dat_down50 = xr.open_dataset(os.path.join(data_dir, diag1_file))
 data_dir = "../interface_height_up/run"
 dat_up = xr.open_dataset(os.path.join(data_dir, diag1_file))
+data_dir = "../interface_height_up50/run"
+dat_up50 = xr.open_dataset(os.path.join(data_dir, diag1_file))
 data_dir = "../no_tide/run1"
 dat = xr.open_dataset(os.path.join(data_dir, diag1_file))
 
-# fig a problem a terrible way
+# fix a problem a terrible way
 dT = dat_down.THETA[0, -1, 0, 0] - dat.THETA[0, -1, 0, 0]
 dat['THETA'] = dat.THETA + dT
 
@@ -46,8 +48,8 @@ dat['THETA'] = dat.THETA + dT
 # Does out fix work? The lines should be the same at the bottom.
 
 # %%
-dats = [dat_down, dat_down50, dat_up, dat]
-names = ['down 100 m', 'down 50 m', 'up 100 m', 'no change']
+dats = [dat_down, dat_down50, dat_up, dat_up50, dat]
+names = ['down 100 m', 'down 50 m', 'up 100 m', 'up 50 m', 'no change']
 
 izs = 60
 
@@ -80,7 +82,7 @@ drF = grid.drF.data
 # HFacC = grid.HFacC[:, :, 0].data
 HFacS = grid.HFacS[:, :, 0].data
 v_ = dat.VVEL[:, :, :, 0].data
-vc = 0.5*(v_[:, :, 1:]*HFacS[:, 1:] + v_[:, :, :-1]*HFacS[:, :-1])
+# vc = 0.5*(v_[:, :, 1:]*HFacS[:, 1:] + v_[:, :, :-1]*HFacS[:, :-1])
 v = v_[it0:, :, iy0:iy1].mean(axis=0)
 
 T = dat.THETA[it0:, :, iy0:iy1, 0].data.mean(axis=0)
@@ -106,12 +108,18 @@ for iy in range(iy1-iy0):
 zs = utils.butter_filter(z, 1/1000, 1/20)
 
 fig, ax = plt.subplots(1, 1)
-ax.contourf(y, Z[70:], T[70:, :], np.arange(0.6, 1.15, 0.05), extend='both', cmap='inferno')
-ax.fill_between(y, ax.get_ylim()[0], -grid.Depth[iy0:iy1, 0], color='grey')
-ax.plot(y, z, lw=3, zorder=2)
-ax.plot(y, zs, lw=3, zorder=1)
+C = ax.contourf((y-y[0])/1000, Z[70:], T[70:, :], np.arange(0.6, 1.15, 0.05), extend='both', cmap='inferno')
+ax.fill_between((y-y[0])/1000, ax.get_ylim()[0], -grid.Depth[iy0:iy1, 0], color='grey')
+# ax.plot(y, z, lw=3, zorder=2)
+ax.plot((y-y[0])/1000, zs, lw=3, zorder=1)
+cb = plt.colorbar(C)
+cb.set_label('Temperature (deg C)')
 
 A = np.sum(np.sqrt((y[1:] - y[:-1])**2 + (z[1:] - z[:-1])**2))
+ax.set_xlabel('Distance (km)')
+ax.set_ylabel('Height (m)')
+fig.savefig('model_isotherms.png', dpi=300,
+            bbox_inches='tight', pad_inches=0)
 
 print("Length = {:1.0f} m".format(A))
 
@@ -184,9 +192,9 @@ plt.plot(np.arange(0.7, 1.1, 0.05), ZT(np.arange(0.7, 1.1, 0.05)))
 # %%
 iy0 = 1000
 iy1 = 2000
-it0 = -100
-Tcs = np.arange(0.7, 0.91, 0.01)
-models = [dat_down, dat_down50, dat, dat_up]
+it0 = slice(250, 350)
+Tcs = np.arange(0.7, 0.84, 0.005)
+models = [dat_down, dat_down50, dat, dat_up50] #, dat_up]
 
 A = np.zeros((len(Tcs), len(models)))
 Vi = np.zeros((len(Tcs), len(models)))
@@ -202,9 +210,9 @@ for j, model in enumerate(models):
     print("Model {}".format(j))
 
     v_ = model.VVEL[:, :, :, 0]
-    vc = 0.5*(v_[:, :, 1:]*HFacS[:, 1:] + v_[:, :, :-1]*HFacS[:, :-1])
-    v = v_[it0:, :, iy0:iy1].mean(axis=0).data
-    T = model.THETA[it0:, :, iy0:iy1, 0].mean(axis=0).data
+#     vc = 0.5*(v_[:, :, 1:]*HFacS[:, 1:] + v_[:, :, :-1]*HFacS[:, :-1])
+    v = v_[it0, :, iy0:iy1].mean(axis=0).data
+    T = model.THETA[it0, :, iy0:iy1, 0].mean(axis=0).data
 
     for i, Tc in enumerate(Tcs):
         ino0 = T[:, 0] < Tc
@@ -240,7 +248,7 @@ for model in models:
 # Plot up the variables.
 
 # %%
-mnames = ['down 100 m', 'down 50 m', 'no change', 'up 100 m']
+mnames = ['- 100 m', '- 50 m', '0 m', '+ 50 m'] #, '+ 100 m']
 
 fig, axs = plt.subplots(1, 4, sharey=True, figsize=(10, 10))
 
@@ -248,28 +256,59 @@ zcs = np.zeros((len(Tcs), len(models)))
 for i in range(len(models)):
     zcs = ZTs[i](Tcs)
 
-axs[0].plot(A[:, :-1], zcs)
-axs[1].plot(Vi[:, :-1], zcs, label='Vi')
-axs[1].plot(Vo[:, :-1], zcs, label='Vo')
-axs[2].plot(w[:, :-1], zcs)
-axs[3].plot(Tr[:, :-1], zcs)
+axs[0].plot(A, Tcs)
+axs[1].plot(Vi, Tcs, label='Vi')
+axs[1].plot(Vo, Tcs, label='Vo')
+axs[2].plot(w, Tcs)
+axs[3].plot(Tr, Tcs)
 
 axs[0].set_xlabel('Length of contour (m)')
-axs[0].set_ylabel('T (deg C)')
+axs[0].set_ylabel('z')
 axs[1].set_xlabel('Area rate in/out (m^2/s)')
 axs[2].set_xlabel('Entrainment velocity (m/s)')
 axs[3].set_xlabel('Transformation rate (m^2/s)')
+axs[3].grid()
 
-axs[0].legend(mnames[:-1])
+axs[0].legend(mnames)
 
-# %% [markdown]
-#
+# %%
+fig, ax = plt.subplots(1, 1, figsize=(3, 4))
+# axs[0].plot(Vi[:, (0, -1)], Tcs)
+ax.plot(Tr[:, (0, -1)], Tcs)
+ax.set_xlabel('Transformation rate (m^2/s)')
+ax.set_ylabel('Temperature (deg C)')
+
+ax.legend(['- 100 m',  '+ 50 m'])
+
+fig.savefig('transformation.png', dpi=300,
+            bbox_inches='tight', pad_inches=0)
+
+# %%
+fig, ax = plt.subplots(1, 1, figsize=(3, 4))
+# axs[0].plot(Vi[:, (0, -1)], Tcs)
+# ax.plot(np.diff(Tr[:, (0, -1)], axis=0), utils.mid(Tcs))
+# ax.set_xlabel('Transformation rate (m^2/s)')
+ax.set_ylabel('Temperature (deg C)')
+
+ax.legend(['- 100 m',  '+ 50 m'])
+
+Tbins = np.arange(0.7, 0.84, 0.02)
+Tbinm = utils.mid(Tbins)
+Tave, _, _ = utils.nan_binned_statistic(utils.mid(Tcs), np.diff(Tr[:, 0]), bins=Tbins)
+ax.step(Tave, Tbinm, where='mid')
+Tave, _, _ = utils.nan_binned_statistic(utils.mid(Tcs), np.diff(Tr[:, -1]), bins=Tbins)
+ax.step(Tave, Tbinm, where='mid')
+
+ax.legend(['- 100 m',  '+ 50 m'])
+
+# fig.savefig('transformation.png', dpi=300,
+#             bbox_inches='tight', pad_inches=0)
 
 # %% [markdown]
 # ## Volume transport comparison between models
 
 # %%
-mnames = ['down 100 m', 'down 50 m', 'no change', 'up 100 m']
+mnames = ['- 100 m', '- 50 m', '0 m', '+ 50 m', '+ 100 m']
 
 fig, ax = plt.subplots(1, 1, sharey=True, figsize=(10, 10))
 
@@ -283,3 +322,8 @@ ax.set_ylabel('z (m)')
 ax.set_xlabel('Area rate in/out (m^2/s)')
 
 ax.legend(mnames[:-1])
+
+# %%
+dat_up50.VVEL[:, 50, 1500, 0]
+
+# %%
